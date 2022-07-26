5b0bf0fa6a44a187157623d3fcfff69bf70aeceb
Ignacio Isas <ignacioisas8@gmail.com>
2022-07-26 19:26:53 -0300

    style

--- src/App.js
+++ src/App.js
@@ -16,12 +16,12 @@ const CardFlip = () => {
   return (
     <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
       <button onClick={handleClick}>
-        <div className="font-serif flex items-center justify-center w-60 h-96 px-10 text-2xl transition transform bg-gradient-to-r from-yellow-300 via-purple-500 to-blue-500 rounded-2xl border-4 border-pink-300">
+        <div className="font-serif text-white flex items-center justify-center w-60 h-96 px-10 text-2xl transition transform bg-gradient-to-r from-yellow-300 via-purple-500 to-blue-500 rounded-2xl border-4 border-pink-300">
         TEMAS DE CONVERSACIÓN</div>
       </button>
       <button onClick={handleClick}>
         <div className="flex items-center justify-center w-60 h-96 rounded-2xl border-4 border-pink-300">
-          <div className="font-serif text-xl px-2">{question}</div>
+          <div className="font-serif text-xl px-2 text-black">{question}</div>
         </div>
       </button>
     </ReactCardFlip>
